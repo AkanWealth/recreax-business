2d20c4d733eb426790f509cacac3c943364aa662
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-05-16 09:15:31 +0100

    Refactor ProductCard component to improve layout and structure; add owner details and adjust styling for better presentation

--- business/src/components/homecomps/minicomps/ProductCard.tsx
+++ business/src/components/homecomps/minicomps/ProductCard.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 
-
 interface ProductCardProps {
   image: string;
   title: string;
@@ -32,15 +31,36 @@ function ProductCard({
       />
       <div className="flex flex-col items-start justify-start gap-4">
         <div className="flex flex-row items-center justify-between">
-          <h3 className="text-2xl font-bold font-plus-jakarta-sans">{title} </h3>
+          <h3 className="text-2xl font-bold font-plus-jakarta-sans">
+            {title}{" "}
+          </h3>
           <a href={link} target="_blank" rel="noopener noreferrer">
             <Image src={companyLogo} alt={title} width={50} height={50} />
           </a>
         </div>
+        <div className="flex flex-col items-start justify-start gap-2">
+          <p className="text-lg font-plus-jakarta-sans text-[#65605c]">
+            {review}
+          </p>
+          <div className="flex flex-row items-center justify-start">
+            <Image
+              src={companyLogo}
+              alt={title}
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
 
-        <p className="text-lg font-plus-jakarta-sans text-[#65605c]">
-          {review}
-        </p>
+            <div className="flex flex-col items-start justify-start gap-1">
+              <p className="text-lg font-semibold font-plus-jakarta-sans text-[#2a2829]">
+                {ownerName}
+              </p>
+              <p className="text-base font-normal  font-plus-jakarta-sans text-[#65605c]">
+                {ownerRole}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
