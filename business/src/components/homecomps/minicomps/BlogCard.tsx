8de6bc429303599654bef5b5f5fbc73d0e7b531e
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-05-18 22:00:05 +0100

    Refactor layout of contact and accelerator pages, improve styling and animations for enhanced user experience; update component structure for better integration

---
+++ business/src/components/homecomps/minicomps/BlogCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Image from "next/image";
+
+type BlogCardProps = {
+  image: string;
+  title: "founder" | "execution";
+  description: string;
+  date: string;
+  readTime: string;
+  type: string;
+};
+
+function BlogCard({ image, title, date, readTime, type }: BlogCardProps) {
+  return (
+    <div className="flex flex-col w-fit gap-8 max-w-[370px] h-fitrounded-2xl border border-gray-200">
+      <Image
+        src={image}
+        alt={title}
+        width={370}
+        height={200}
+        className="rounded-2xl"
+      />
+      <div className="flex flex-col items-start justify-start gap-4 ">
+        <p
+          className={`text-sm flex items-center gap-2 ${
+            type === "founder"
+              ? "text-[#0671e0]"
+              : type === "executive"
+              ? "text-[#ff6644]"
+              : "text-gray-500"
+          }`}
+        >
+          {(type === "founder" || type === "executive") && (
+            <span
+              className={`w-2 h-2 rounded-full ${
+                type === "founder" ? "bg-[#0671e0]" : "bg-[#ff6644]"
+              }`}
+            ></span>
+          )}
+          {type}
+        </p>
+        <p className="text-sm text-gray-500">{date}</p>
+        <div className="flex flex-col gap-2">
+          <h4 className="font-tomato font-semibold text-lg">{title}</h4>
+
+          <p className="text-sm text-gray-500">{readTime}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
