619ae88caf95a49cf97ced94e8f16c183fca594b
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-06-03 11:12:49 +0100

    Refactor Products and Works components to enhance layout and visual engagement; replace static images with Lottie animations for improved performance and user experience.

--- src/components/homecomps/homepage/Works.tsx
+++ src/components/homecomps/homepage/Works.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "motion/react";
+import Lottie from "../minicomps/Lottie";
+import aniShare from "./Share.json";
+import aniPlanning from "./Planning.json";
+import aniLaunch from "./Launch.json";
 
 function Works() {
   return (
@@ -42,13 +46,9 @@ function Works() {
               execution plan
             </p>
           </div>
-          <Image
-            src="/images/Works-1.svg"
-            alt="works-1"
-            width={342}
-            height={259}
-            className="w-full md:w-auto"
-          />
+          <div className="w-full h-full max-w-[342px]">
+            <Lottie data={aniShare} loop={true} />
+          </div>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
@@ -69,13 +69,9 @@ function Works() {
               feedback and alignment
             </p>
           </div>
-          <Image
-            src="/images/Works-2.svg"
-            alt="works-2"
-            width={352}
-            height={244}
-            className="w-full md:w-auto"
-          />
+          <div className="w-full h-full max-w-[352px]">
+            <Lottie data={aniPlanning} loop={true} />
+          </div>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, x: 50 }}
@@ -96,13 +92,9 @@ function Works() {
               beyond launch.
             </p>
           </div>
-          <Image
-            src="/images/Works-3.svg"
-            alt="works-3"
-            width={342}
-            height={259}
-            className="w-full md:w-auto"
-          />
+          <div className="w-full h-full max-w-[342px]">
+            <Lottie data={aniLaunch} loop={true} />
+          </div>
         </motion.div>
       </div>
     </div>
