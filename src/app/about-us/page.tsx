cd03dff2d006adcaef16a963ef694e4cccf974e3
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-06-18 16:20:45 +0100

    Comment out the Origin component in the About Us page to streamline content presentation and improve code clarity. This change enhances maintainability by removing unused elements while keeping the option to reintegrate them in the future.

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AboutMetrics from "@/components/homecomps/aboutcomps/AboutMetrics";
 import CICS from "@/components/homecomps/aboutcomps/CICS";
-import Origin from "@/components/homecomps/aboutcomps/Origin";
+// import Origin from "@/components/homecomps/aboutcomps/Origin";
 import AboutHero from "@/components/homecomps/aboutcomps/AboutHero";
 function page() {
   return (
     <div>
       <AboutHero />
-      <Origin />
+      {/* <Origin /> */}
       <CICS />
       <AboutMetrics />
     </div>
